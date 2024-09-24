190844fc02d8ad67be0d81fe599b87556fe806d2
DESKTOP-4R5H82B\youar <youareshine.inmylife@gmail.com>
2024-09-24 07:51:41 +0700

    Update db_connect

---
+++ api/db_connect.php
@@ -0,0 +1,15 @@
+<?php
+// Thông tin kết nối cơ sở dữ liệu
+$servername = "localhost";
+$username = "root"; // Tên người dùng MySQL
+$password = ""; // Mật khẩu của MySQL
+$dbname = "company"; // Tên cơ sở dữ liệu
+
+// Tạo kết nối
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if ($conn->connect_error) {
+    die("Kết nối thất bại: " . $conn->connect_error);
+}
+?>
