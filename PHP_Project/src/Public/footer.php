61598f6f4a7bf93e779f5c0ec610d25deaf10203
DESKTOP-662K8FQ\Admin <nguyenduchiep1512@gmail.com>
2024-10-01 00:44:20 +0700

    Update Footer

--- PHP_Project/src/Public/footer.php
+++ PHP_Project/src/Public/footer.php
@@ -1,96 +1,172 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="src/Public/footer.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="demoft.css">
     <title>Footer</title>
 </head>
-
 <body>
-    <footer>
-        <nav class="navbar">
-            <div class="footer">
-                <h3>Categories</h3>
-                <ul>
-                    <li><a href="">Anti-Aging & Wrinkle Treatments</a></li>
-                    <li><a href="">Eye Care</a></li>
-                    <li><a href="">Face Creams</a></li>
-                    <li><a href="">Serums</a></li>
-                </ul>
-            </div>
-            <div class="footer">
-                <h3>Customer Services</h3>
-                <ul>
-                    <li><a href="">FAQ</a></li>
-                    <li><a href="">Shipping Options</a></li>
-                    <li><a href="">Returns Policy</a></li>
-                    <li><a href="">Payment</a></li>
-                    <li><a href="">Track Your Order</a></li>
-                    <li><a href="">Authorised Retailers</a></li>
-                    <li><a href="">Student Discount</a></li>
-                    <li><a href="">Cookies Policy</a></li>
-                </ul>
-            </div>
-        </nav>
-        <nav class="navbar">
-            <div class="footer">
-                <h3>Most Popular</h3>
-                <ul>
-                    <li><a href="">Double Serum</a></li>
-                    <li><a href="">Hand and Nail Treatment Cream</a></li>
-                    <li><a href="">Hydrating Toning Lotion</a></li>
-                    <li><a href="">Instant Eye Make Up Remover</a></li>
-                </ul>
-            </div>
-            <div class="footer">
-                <h3>About Clarins</h3>
-                <ul>
-                    <li><a href=""> About Clarins Group</a></li>
-                    <li><a href="">Our Story/Commitment</a></li>
-                    <li><a href="">Loyalty Program</a></li>
-                    <li><a href="">Black Friday</a></li>
-                    <li><a href="">Cyber Monday</a></li>
-                    <li><a href="">Spa</a></li>
-                    <li><a href="">Find A Store</a></li>
-                    <li><a href="">Beauty FAQ</a></li>
-                    <li><a href="">Careers</a></li>
-                    <li><a href="">Gender Pay Report</a></li>
-                    <li><a href="">Tax Strategy</a></li>
-                    <li><a href="">Modern Slavery And Human Trafficking Statement</a></li>
-                </ul>
-            </div>
-        </nav>
-        <nav class="navbar">
-            <div class="footer">
-                <h3>WHAT'S NEW</h3>
-                <ul>
-                    <li><a href=""> Limited Edition Lip Comfort Oil</a></li>
-                    <li><a href="">Body Fit Active</a></li>
-                    <li><a href="">Multi-Active Day Cream SPF 15</a></li>
-                </ul>
-            </div>
-            <div class="footer">
-                <h3>SIGN UP TO OUR NEWSLETTER</h3>
-                <p>Unlock 10% off your first order</p>
-                <form>
-                    <div>
-                        <input type="text" placeholder="Email addrees" required>
+    <footer class="footer-section">
+        <div class="container">
+            <div class="footer-contact pt-5 pb-5">
+                <div class="row">
+                    <div class="col-xl-4 col-md-4 mb-30">
+                        <div class="single-contact">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="contact-text">
+                                <h4>Find us</h4>
+                                <span>1234 Hoàn Kiếm, Hà nội, VN</span>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <button>Subscribe</button>
+                    <div class="col-xl-4 col-md-4 mb-30">
+                        <div class="single-contact">
+                            <i class="fas fa-phone"></i>
+                            <div class="contact-text">
+                                <h4>Call us</h4>
+                                <span>0123456789</span>
+                            </div>
+                        </div>
                     </div>
-                </form>
+                    <div class="col-xl-4 col-md-4 mb-30">
+                        <div class="single-contact">
+                            <i class="far fa-envelope-open"></i>
+                            <div class="contact-text">
+                                <h4>Mail us</h4>
+                                <span>mail@info.com</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="footer">
-                <h1>Need help?</h1>
-                <p>Email or call us at 01279 967 776</p>
-                <p>Monday - Friday (excluding holidays)</p>
-                <p>9:00 AM - 5:30 PM</p>
+            <div class="footer-content pt-5 pb-5">
+                <div class="row">
+                    <!-- nếu muốn thì có thể hiện logo và cả các liên kết từ các trang như fb,X,gg ở dưới footer -->
+                    <!-- <div class="col-xl-4 col-lg-4 mb-50">
+                        <div class="footer-widget">
+                            <div class="footer-logo">
+                                <a href="index.html"><img src="https://i.ibb.co/QDy827D/ak-logo.png" class="img-fluid" alt="logo"></a>
+                            </div>
+                            <div class="footer-text">
+                                <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing
+                                elit,Lorem ipsum dolor sit amet.</p>
+                            </div>
+                            <div class="footer-social-icon">
+                                <span>Follow us</span>
+                                <a href="#"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                                <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
+                                <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
+                            </div>
+                        </div>
+                    </div> -->
+                    <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                        <div class="footer-widget">
+                            <div class="footer-widget-heading">
+                                <h3>Categories</h3>
+                            </div>
+                            <ul>
+                                <li><a href="">Anti-Aging & Wrinkle Treatments</a></li>
+                                <li><a href="">Eye Care</a></li>
+                                <li><a href="">Face Creams</a></li>
+                                <li><a href="">Serums</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                        <div class="footer-widget">
+                            <div class="footer-widget-heading">
+                                <h3>Customer Services</h3>
+                            </div>
+                            <ul>
+                                <li><a href="">FAQ</a></li>
+                                <li><a href="">Shipping Options</a></li>
+                                <li><a href="">Returns Policy</a></li>
+                                <li><a href="">Payment</a></li>
+                                <li><a href="">Track Your Order</a></li>
+                                <li><a href="">Authorised Retailers</a></li>
+                                <li><a href="">Student Discount</a></li>
+                                <li><a href="">Cookies Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                        <div class="footer-widget">
+                            <div class="footer-widget-heading">
+                                <h3>Most Popular</h3>
+                            </div>
+                            <ul>
+                                <li><a href="">Double Serum</a></li>
+                                <li><a href="">Hand and Nail Treatment Cream</a></li>
+                                <li><a href="">Hydrating Toning Lotion</a></li>
+                                <li><a href="">Instant Eye Make Up Remover</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    
+                    <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                        <div class="footer-widget">
+                            <div class="footer-widget-heading">
+                                <h3>About Clarins</h3>
+                            </div>
+                            <ul>
+                                <li><a href="">About Us</a></li>
+                                <li><a href="">Our Story/Commitment</a></li>
+                                <li><a href="">Loyalty Program</a></li>
+                                <li><a href="">Black Friday</a></li>
+                                <li><a href="">Cyber Monday</a></li>
+                                <li><a href="">Spa</a></li>
+                                <li><a href="">Find A Store</a></li>
+                                <li><a href="">Beauty FAQ</a></li>
+                                <li><a href="">Careers</a></li>
+                                <li><a href="">Gender Pay Report</a></li>
+                                <li><a href="">Tax Strategy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
+                        <div class="footer-widget">
+                            <div class="footer-widget-heading">
+                                <h3>SIGN UP TO OUR NEWSLETTER</h3>
+                            </div>
+                            <div class="footer-text mb-25">
+                                <p>Unlock 10% off your first order.</p>
+                            </div>
+                            <div class="subscribe-form">
+                                <form action="#">
+                                    <input type="text" placeholder="Email Address">
+                                    <button><i class="fa-solid fa-paper-plane"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </nav>
+        </div>
+        <div class="copyright-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl col-lg text-lg-left">
+                        <div class="copyright-text">
+                            <p>&copy; Clarins copyright. All Rights Reserved.</p>
+                        </div>
+                    </div>
+                    <!-- <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                        <div class="footer-menu">
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Terms</a></li>
+                                <li><a href="#">Privacy</a></li>
+                                <li><a href="#">Policy</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+        </div>
     </footer>
 </body>
-
 </html>
+
